83a27e933cadf96e2eb8415866cfe2e7d0b4333b
Johan <a19204146@alumnos.uady.mx>
2026-05-19 20:02:33 -0600

    MANT-65 fix(infra): Inicializa django.setup antes de importar rutas de canales virtuales

--- CE/asgi.py
+++ CE/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CE.settings')
+
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
